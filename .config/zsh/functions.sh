ce9ac8f4f0ee71495c676cbffa993dae41594f76
Daniel Herink <work@daniel.herink.com>
2025-02-07 08:30:02 -0800

    Added zsh-completions plugin
    Fixed paths, added functions file, removed auto-complete call that isn't needed

---
+++ .config/zsh/functions.sh
@@ -0,0 +1,9 @@
+# pip zsh completion
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null ))
+}
--- setup-home.sh
+++ setup-home.sh
@@ -2,10 +2,10 @@
 
 echo "Starting Zsh prompt setup: going to install oh-my-zsh and Powerlevel10k and other necessary pieces\n"
 
-local font_home=${HOME}/.local/share/fonts
-local omz_home=${HOME}/.oh-my-zsh
-local zsh_256color_home=$omz_home/custom/plugins/zsh-256color
-local p10k_home=$omz_home/custom/themes/powerlevel10k
+font_home=${HOME}/.local/share/fonts
+omz_home=${HOME}/.oh-my-zsh
+omz_plugins=$omz_home/custom/plugins
+omz_themes=$omz_home/custom/themes
 
 if [[ -e $font_home/MesloLGS_NF_Regular.ttf && -e $font_home/MesloLGS_NF_Bold.ttf && -e $font_home/MesloLGS_NF_Regular.ttf && -e $font_home/MesloLGS_NF_Bold_Italic.ttf ]]; then
     echo "Found the patched MesloLGS NF fonts in $font_home"
@@ -31,22 +31,30 @@ fi
 
 echo ""
 
-if [[ ! -d $zsh_256color_home ]]; then
+if [[ ! -d "$omz_plugins/zsh-256color" ]]; then
     echo "Going to install zsh-256color..."
-    git clone https://github.com/chrissicool/zsh-256color $zsh_256color_home
+    git clone https://github.com/chrissicool/zsh-256color "$omz_plugins/zsh-256color"
 else
-    echo "Found zsh-256color installation at $zsh_256color_home"
+    echo "Found zsh-256color installation at $omz_plugins/zsh-256color"
 fi
 
 echo ""
 
-if [[ ! -d $p10k_home ]]; then
-    echo "Going to install Powerlevel10k..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $p10k_home
+if [[ ! -d "$omz_plugins/zsh-completions" ]]; then
+    echo "Going to install zsh-completions..."
+    git clone https://github.com/zsh-users/zsh-completions "$omz_plugins/zsh-completions"
 else
-    echo "Found Powerleve10k installation at $p10k_home"
+    echo "Found zsh-completions at $omz_plugins/zsh-completions"
 fi
 
-echo "\nCleaning up the unneeded Git files"
-rm -rf LICENSE .git/
+echo ""
+
+if [[ ! -d "$omz_themes/powerlevel10k" ]]; then
+    echo "Going to install Powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$omz_themes/powerlevel10k"
+else
+    echo "Found Powerleve10k installation at $omz_themes/powerlevel10k"
+fi
+
+echo ""
 echo "Done"
