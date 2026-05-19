731d4f174c1af971e051815375d09590723739e4
Daniel Herink <work+github@danielherink.com>
2026-05-19 16:46:53 -0700

    Adding OSC 52 Clipboard Handling to Nvim + Tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,21 @@ vim.opt.number = true
 vim.opt.mouse = 'a'
 vim.opt.clipboard = 'unnamedplus'
 
+-- Use OSC 52 for clipboard so yank/paste work over SSH+tmux without an X
+-- server. Avoids "Can't open display" errors when DISPLAY is stale or absent.
+-- Requires Neovim 0.10+ and a terminal that supports OSC 52 (MinTTY does).
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
